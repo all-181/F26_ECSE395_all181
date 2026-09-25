0f9bc7d5c6443a5c214225983ba11a384be31960
all-181 <all181@case.edu>
2026-09-25 12:31:15 -0400

    Code for Lab 5 before physical build

--- Lab 5/integration_exploration.md
+++ Lab 5/integration_exploration.md
@@ -3,22 +3,63 @@
 
 **Assignment Overview:**
 
+I intend to design a stopwatch.
+
 **Repository Overview:**
 
 **Steps:**
 
+First, I read over example code for each new component I would be using.
+
+OLED:
+Tutorial Site: https://docs.sunfounder.com/projects/umsk/en/latest/03_esp32/esp32_lesson27_oled.html#esp32-lesson27-oled 
+Wiring diagram is found on tutorial site.
+Adafruit SSD1306 Library: Designed to help with interfacing of SSD1306 OLED display.  
+Adafruit GFX Library: Core graphics library for displaying text, colors, shapes, etc.  This will be necessary for displaying the intended stopwatch numbers.
+Sample code, which my code is based on, is found on tutorial site. 
+
+Button:
+Tutorial Site: https://docs.sunfounder.com/projects/umsk/en/latest/03_esp32/esp32_lesson01_button.html#eps32-lesson01-button
+Wiring diagram is found on tutorial site.
+No additional libraries are needed.
+When pressed, the button closes the circuit.  When unpressed, the button leaves an open circuit.
+
+Vibration Sensor:
+Tutorial Site: https://docs.sunfounder.com/projects/umsk/en/latest/03_esp32/esp32_lesson24_vibration_sensor.html#esp32-lesson24-vibration-sensor
+Wiring diagram is found on tutorial site.
+This is a digital sensor that will go high (1) when the amplitude of a vibration exceeds the reference voltage set, and otherwise will go low (0).
+
+Next, I wrote an algorithm for my stopwatch.
+
+1. Initial state is not counting and has "Ready to go!" displayed.
+2. Check if button is pressed.  If button is pressed, go to step 3.  If not, go to step 1.
+3. Display "3" for 1 second.
+4. Display "2" for 1 second.
+5. Display "1" for 1 second.
+6. Display "0.0"
+7. Count up every tenth of a second (0.1s = 100 ms).  For example, 0.1 seconds after step 6, display "0.01".  
+8. Check if vibration module is pressed.  
+9. If pressed, display current time for 5 seconds, then go to step 1.  If not pressed, go to step 7.
+
+
+
+
+
+
 **Setup and Preparation:**
 I am using the Vibration Sensor Module (SW-420).
 This module can detect vibrations or shocks on a surface.
+I am also using the Button Module.
+The button module detects the state of a button.
 I am using the OLED Display Module (SSD1306).
 This module can display text, graphics, and images.
 
 **In-Class Task Description:**
 
 **Documentation:**
-Sensor: https://docs.sunfounder.com/projects/umsk/en/latest/01_components_basic/24-component_vibration.html
-Actuator:
-
+Vibration Sensor: https://docs.sunfounder.com/projects/umsk/en/latest/01_components_basic/24-component_vibration.html
+OLED: https://docs.sunfounder.com/projects/umsk/en/latest/01_components_basic/27-component_oled.html 
+Button: https://docs.sunfounder.com/projects/umsk/en/latest/01_components_basic/01-component_button.html 
 **Tools Used:**
 
 **Upload Process:**
--- Lab 5/src/main.cpp
+++ Lab 5/src/main.cpp
@@ -1,9 +1,112 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h> // includes required library
+#include <adafruit_GFX.h> // includes required library
+
+
+// OLED STUFF
+#define SCREEN_WIDTH 128 // display width in pixels
+#define SCREEN_HEIGHT 6 // display height in pixels
+
+#define OLED_RESET -1 // all181: reset pin number
+#define SCREEN_ADDRESS 0x3C // all181: 
+
+Adafruit_SSD1306
+display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+// BUTTON STUFF
+const int buttonPin = 26; // all181:
+const int ledPin = 25; // all181: 
+int buttonState = 0; // variable to hold current state of button
+
+// VIBRATION SENSOR STUFF
+const int sensorPin = 34; // all181:
+
 
 void setup() {
+    // OLED
+    Serial.begin(9600); //initialize baud rate for OLED
 
-}
+        display.clearDisplay();
+        display.setTextSize(1); // sets text size
+        display.setTextColor(WHITE); // sets text color
+        display.setCursor(0,28); // sets cursor position
+        display.println("Ready?"); // display on screen
+        delay(2000); // wait 2s
+        display.clearDisplay(); // clear screen
+
+    // Button
+    pinMode(buttonPin, INPUT); // makes button an input
+    pinMode(ledPin, OUTPUT); // all181:
+
+    // Vibration Sensor
+    pinMode(sensorPin, INPUT); // makes vibration sensor an input
+
+    }
+
+
 
 void loop() {
+
+
+    // 1) display opening message
+    display.clearDisplay();
+        display.setTextSize(1); // sets text size
+        display.setTextColor(WHITE); // sets text color
+        display.setCursor(0,28); // sets cursor position
+        display.println("Ready to go!"); // display on screen
+        display.display();
+        
+
+    // 2) Check if button is pressed.  If pressed, go to step 3.  If not, loop back.
+    buttonState = digitalRead(buttonPin); // HIGH is pressed, LOW is not pressed
+
+    if (buttonPin == HIGH){
+
+        // 3-5) Display "3...2...1..."
+        display.clearDisplay();
+        display.println("3");
+        display.display();
+        delay(1000);
+        display.clearDisplay();
+        display.println("2");
+        display.display();
+        delay(1000);
+        display.clearDisplay();
+        display.println("1");
+        display.display();
+        delay(1000);
+        display.clearDisplay();
+
+
+        // 6) Display "0.0"
+        display.println("0.0");
+        display.display();
+        float watchTime = 0.0;
+
+        while (digitalRead(sensorPin) == 0){
+            watchTime = watchTime + 0.1; // increment watch time
+            display.println(watchTime,1);
+            display.display(); // display watch time, specify 1 decimal place
+            delay(100); // delay 100ms = 0.1s
+            display.clearDisplay();
+
+        }
+
+        // now, vibration module has been set off
+        // display current time for 5 seconds
+        display.println(watchTime);
+        display.display();
+        delay(5000);
+        display.clearDisplay();
+
+    } else {
+        delay(100); // all181: add delay of 0.1s between checking if button pressed
+
+    }
+
 
 }
