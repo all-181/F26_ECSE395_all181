2dc8e20935088e4702a8ba66e0ccb6e228d91913
all-181 <all181@case.edu>
2026-09-25 14:04:41 -0400

    Adjusted Lab 5 code and comments

--- Lab 5/src/main.cpp
+++ Lab 5/src/main.cpp
@@ -1,49 +1,54 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_SSD1306.h> // includes required library
-#include <adafruit_GFX.h> // includes required library
+#include <Adafruit_SSD1306.h> // all181: includes required library
+#include <adafruit_GFX.h> // all181: includes required library
 
 
-// OLED STUFF
-#define SCREEN_WIDTH 128 // display width in pixels
-#define SCREEN_HEIGHT 6 // display height in pixels
+// OLED 
+#define SCREEN_WIDTH 128 // all181: display width in pixels
+#define SCREEN_HEIGHT 64 //all181: display height in pixels
 
 #define OLED_RESET -1 // all181: reset pin number
-#define SCREEN_ADDRESS 0x3C // all181: 
+#define SCREEN_ADDRESS 0x3C // all181: screen address (commonly 0x3C, try 0x3D if this does not work)
 
-Adafruit_SSD1306
-display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // all181: from test code
 
 
-// BUTTON STUFF
-const int buttonPin = 26; // all181:
-const int ledPin = 25; // all181: 
-int buttonState = 0; // variable to hold current state of button
+// BUTTON 
+const int buttonPin = 26; // all181: connected button sensor to A0
+int buttonState = 0; // all181: variable to hold current state of button
 
-// VIBRATION SENSOR STUFF
-const int sensorPin = 34; // all181:
+// VIBRATION SENSOR 
+const int sensorPin = 25; // all181: connected vibration sensor to A1
 
 
 void setup() {
     // OLED
-    Serial.begin(9600); //initialize baud rate for OLED
+    Serial.begin(9600); // all181: initialize baud rate for OLED; different from serial monitor
 
-        display.clearDisplay();
-        display.setTextSize(1); // sets text size
-        display.setTextColor(WHITE); // sets text color
-        display.setCursor(0,28); // sets cursor position
-        display.println("Ready?"); // display on screen
-        delay(2000); // wait 2s
-        display.clearDisplay(); // clear screen
+    // all181: check to see if we can write to the OLED screen
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)){
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+        ;
+    }
+        // Check to see if OLED works
+        // all181: These same commands are commonly repeated.  Comments for their functionality are here.
+        display.clearDisplay(); // all181: wipes any existing input off of display
+        display.setTextSize(1); // all181: sets text size
+        display.setTextColor(WHITE); // all181: sets text color
+        display.setCursor(0,28); // all181: sets position where text starts (middle left of screen)
+        display.println("Loading..."); // all181: write what to display on screen
+        display.display(); // all181: displays on screen
+        delay(2000); // all181: wait 2s
+        display.clearDisplay(); 
 
     // Button
-    pinMode(buttonPin, INPUT); // makes button an input
-    pinMode(ledPin, OUTPUT); // all181:
+    pinMode(buttonPin, INPUT); // all181: makes button an input
 
     // Vibration Sensor
-    pinMode(sensorPin, INPUT); // makes vibration sensor an input
+    pinMode(sensorPin, INPUT); // all181: makes vibration sensor an input
 
     }
 
@@ -51,53 +56,63 @@ void setup() {
 
 void loop() {
 
-
-    // 1) display opening message
+    // all181: 1) display opening message
     display.clearDisplay();
         display.setTextSize(1); // sets text size
         display.setTextColor(WHITE); // sets text color
+        display.setTextSize(1); // sets text size
         display.setCursor(0,28); // sets cursor position
-        display.println("Ready to go!"); // display on screen
+        display.print("Ready to go!"); // display on screen
         display.display();
         
 
-    // 2) Check if button is pressed.  If pressed, go to step 3.  If not, loop back.
-    buttonState = digitalRead(buttonPin); // HIGH is pressed, LOW is not pressed
+    // all181: 2) Check if button is pressed.  If pressed, go to step 3.  If not, loop back.
+    buttonState = digitalRead(buttonPin); // all181: read button value
 
-    if (buttonPin == HIGH){
+    if (buttonState == LOW){ // all181: if button is pressed
 
-        // 3-5) Display "3...2...1..."
+        //all181: 3-5) Display "3...2...1..."
         display.clearDisplay();
-        display.println("3");
+        display.setTextSize(2); // sets text size
+        display.setCursor(0,28); // sets cursor position
+        display.print("3");
         display.display();
-        delay(1000);
+        delay(1000); // all181: wait a second
         display.clearDisplay();
-        display.println("2");
+        display.setTextSize(2); // sets text size
+        display.setCursor(0,28); // sets cursor position
+        display.print("2");
         display.display();
-        delay(1000);
+        delay(1000); // all181: wait a second
         display.clearDisplay();
-        display.println("1");
+        display.setTextSize(2); // sets text size
+        display.setCursor(0,28); // sets cursor position
+        display.print("1");
         display.display();
-        delay(1000);
+        delay(1000); // all181: wait a second
         display.clearDisplay();
 
 
-        // 6) Display "0.0"
-        display.println("0.0");
+        // all181: 6) Display "0.0"
+        display.setTextSize(2); // sets text size
+        display.setCursor(0,28); // sets cursor position
+        display.print("0.0");
         display.display();
-        float watchTime = 0.0;
+        float watchTime = 0.0; // all181: initialize time variable
 
         while (digitalRead(sensorPin) == 0){
             watchTime = watchTime + 0.1; // increment watch time
-            display.println(watchTime,1);
+            display.setTextSize(2); // sets text size
+            display.setCursor(0,28); // sets cursor position
+            display.print(watchTime,1);
             display.display(); // display watch time, specify 1 decimal place
             delay(100); // delay 100ms = 0.1s
             display.clearDisplay();
 
         }
 
-        // now, vibration module has been set off
-        // display current time for 5 seconds
+        // all181: now, vibration module has been set off
+        // all181: display current time for 5 seconds
         display.println(watchTime);
         display.display();
         delay(5000);
